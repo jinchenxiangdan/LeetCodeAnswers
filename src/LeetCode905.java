f31acec5d420127efdee856b303f3b55f3f28727
jinchenxiangdan <jinchenxiangdan@gmail.com>
2019-06-13 19:19:38 -0700

    Update LeetCode905.java

--- src/LeetCode905.java
+++ src/LeetCode905.java
@@ -1,10 +1,12 @@
 /**
+Author: Shawn Jin
 Runtime: 1 ms, faster than 100.00% of Java online submissions for Sort Array By Parity.
 Memory Usage: 39.4 MB, less than 96.27% of Java online submissions for Sort Array By Parity.
 */
 class LeetCode905 {
     public int[] sortArrayByParity(int[] A) {
     int[] answer = new int[A.length];
+    // e is even number pointer, o is odd number pointer
     int e = 0, o = A.length-1;
     for (int i = 0; i < A.length; i++) {
         // if even numbers 
