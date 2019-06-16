edd40733655ee24ef97239a1aa910346fe00eac0
jinchenxiangdan <jinchenxiangdan@gmail.com>
2019-06-15 22:20:24 -0700

    Update LeetCode657.java

--- src/LeetCode657.java
+++ src/LeetCode657.java
@@ -1,41 +1,28 @@
-import java.util.HashMap;
-import java.util.Map;
-
-public class LeetCode657 {
-
-    private static Map<String, Integer> directionCount = new HashMap<>();
-
-    public static void main(String[] arg){
-        System.out.println(judgeCircle("LL"));
-        System.out.println(judgeCircle("UD"));
-    }
-
-    private static boolean judgeCircle(String moves) {
-        directionCount.put("up&down", 0);
-        directionCount.put("left&right", 0);
-
-        for (int i = 0; i < moves.length(); i++){
-            switch (moves.charAt(i)){
-
+class LeetCode657 {
+    /**
+    Author: Shawn Jin
+    Runtime: 7 ms, faster than 68.17% of Java online submissions for Robot Return to Origin.
+    Memory Usage: 37.7 MB, less than 99.56% of Java online submissions for Robot Return to Origin.
+    */
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch(moves.charAt(i)) {
                 case 'U':
-                    directionCount.put("up&down",directionCount.get("up&down")+1);
+                    y++;
                     break;
                 case 'D':
-                    directionCount.put("up&down",directionCount.get("up&down")-1);
+                    y--;
                     break;
                 case 'L':
-                    directionCount.put("left&right",directionCount.get("left&right")+1);
+                    x--;
                     break;
                 case 'R':
-                    directionCount.put("left&right",directionCount.get("left&right")-1);
+                    x++;
                     break;
-                default:
-                    // This part is just for format.
-                    continue;
             }
+            
         }
-        return directionCount.get("up&down") == 0 && directionCount.get("left&right") == 0;
+        return (x == 0 && y == 0);
     }
-
-
 }
