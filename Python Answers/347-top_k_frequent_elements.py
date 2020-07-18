7b4943d6611761baa90a9610da03b10503fd9991
shawn <jinchenxiangdan@gmail.com>
2020-07-17 23:05:38 -0700

    add leetcode 347

---
+++ Python Answers/347-top_k_frequent_elements.py
@@ -0,0 +1,25 @@
+'''
+Author: Shawn Jin
+'''
+
+class Solution:
+    '''
+    Runtime: 136 ms, faster than 33.83% of Python3 online submissions for Top K Frequent Elements.
+    Memory Usage: 18.4 MB, less than 17.97% of Python3 online submissions for Top K Frequent Elements.
+    '''
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        my_hashtable = {}
+        for item in nums:
+            if item in my_hashtable:
+                my_hashtable[item] += 1
+            else:
+                my_hashtable[item] = 1
+        sorted_hashtable = sorted(my_hashtable.items(), key=lambda item: item[1], reverse=True)
+        return [sorted_hashtable[i][0] for i in range(k)]
+
+    '''
+    Runtime: 136 ms, faster than 33.83% of Python3 online submissions for Top K Frequent Elements.
+    Memory Usage: 18.4 MB, less than 24.68% of Python3 online submissions for Top K Frequent Elements.
+    '''
+    def topKFrequent_oneLine(self, nums: List[int], k: int) -> List[int]:
+        return [x for x,_ in sorted(Counter(nums).items(), key=lambda x:-x[1])[:k]]
--- README.md
+++ README.md
@@ -4,5 +4,4 @@ This repository will have the simple answers for [LeetCode](https://leetcode.com
 However, **The answers in this res may NOT the optimal answers**, if you know better answer, please share your answer with me.
 
 Thanks
-## TODO
-classify question by resolving methods
+
