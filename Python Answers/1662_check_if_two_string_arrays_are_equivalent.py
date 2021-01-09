436ca3da4e9ce8325ed1823e5eb165f8c2904055
Shawn Jin <jinchenxiangdan@gmail.com>
2021-01-08 20:20:03 -0700

    Update 1662_check_if_two_string_arrays_are_equivalent.py

--- Python Answers/1662_check_if_two_string_arrays_are_equivalent.py
+++ Python Answers/1662_check_if_two_string_arrays_are_equivalent.py
@@ -4,13 +4,30 @@ Author: Shawn Jin
 
 """
 
-class Solution(object):
-    
-    def arrayStringsAreEqual(self, word1, word2):
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         """
-        :type word1: List[str]
-        :type word2: List[str]
-        :rtype: bool
+        This method effiency is close to the easiest way shown below
         """
+        input_string_1 = "".join(word1)
+        length = len(input_string_1)
+        index = 0
+        for i in range(len(word2)):
+            for char in word2[i]:
+                if (index >= length or input_string_1[index] != char):
+                    return False
+                else:
+                    index+=1
+        return index == len(input_string_1)
 
-        
+    def arrayStringsAreEqual2(self, word1: List[str], word2: List[str]) -> bool:
+        """
+        Easiest way to compare the two list of string
+        """
+        input_string_1 = "".join(word1)
+        input_string_2 = "".join(word2)
+        return input_string_1 ==  input_string_2
+
+
+if __name__ == "__main__":
+    print(Solution().arrayStringsAreEqual(["ab", "c"], ["a", "bce"]))
