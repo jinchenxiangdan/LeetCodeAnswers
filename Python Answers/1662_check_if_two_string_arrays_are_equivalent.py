70586f758e6681aaa6f7e67ef1474f40b00c8887
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-01-08 19:55:58 -0700

    Create 1662_check_if_two_string_arrays_are_equivalent.py

---
+++ Python Answers/1662_check_if_two_string_arrays_are_equivalent.py
@@ -0,0 +1,16 @@
+from typing import List
+"""
+Author: Shawn Jin
+
+"""
+
+class Solution(object):
+    
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+
+        
