cca47931504081e1bfff2598cc7c5515bc70d9f0
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-01-11 22:31:31 -0700

    Update 127_word_ladder.py

--- Python Answers/127_word_ladder.py
+++ Python Answers/127_word_ladder.py
@@ -26,18 +26,22 @@ class Solution:
 
 
         def couldConvert(lastWord: str, targetWord: str) -> bool:
-            set1 = set()
-            set2 = set()
-            for char in lastWord:
-                set1.add(char)
-            for char in targetWord:
-                set2.add(char)
-            # return len(set1.difference(set2)) == 1
-            if len(set1.difference(set2)) == 1:
-                print(lastWord, "could be convert to", targetWord)
-                return True
-            else:
+            """
+            Check if the two string only have specific number of characters are different.
+            Default difference is { 1 }
+            """
+            # set the difference char
+            REQUIRE_DIFF = 1
+            # comparing
+            if (len(lastWord) != len(targetWord)):
                 return False
+            
+            total_diff = 0
+            for i in range(len(targetWord)):
+                if (targetWord[i] != lastWord[i]):
+                    total_diff += 1
+            return total_diff == REQUIRE_DIFF
+
 
 
         def helper(lastWord, targetWord, checkPool, wordList):
