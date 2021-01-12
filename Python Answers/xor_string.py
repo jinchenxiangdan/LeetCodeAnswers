8327e15ebe0caaf82fdb1deca66309e5bf13f75b
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-01-11 20:17:01 -0700

    Move q127 to python answer

--- Python Answers/xor_string.py
+++ Python Answers/xor_string.py
@@ -10,5 +10,12 @@ class XOR:
         # check length of the two strings
         if (len(string_1) != len(string_2)):
             return -1
+        #
         # 
+        # 
+        #  
 
+if __name__ == "__main__":
+    continue
+    xor = XOR()
+    
