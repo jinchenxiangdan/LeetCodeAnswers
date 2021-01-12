6d7bb6246a98c1fb933ee777520e840f770fb922
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-01-11 22:31:37 -0700

    Update xor_string.py

--- Python Answers/xor_string.py
+++ Python Answers/xor_string.py
@@ -3,19 +3,17 @@
 
 class XOR:
 
-
-
     def xor_string(self, string_1: str, string_2: str):
 
         # check length of the two strings
         if (len(string_1) != len(string_2)):
             return -1
         #
-        # 
+        
         # 
         #  
 
 if __name__ == "__main__":
-    continue
     xor = XOR()
-    
+
+
