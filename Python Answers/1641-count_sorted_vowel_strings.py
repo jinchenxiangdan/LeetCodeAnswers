9527d87f94bc4a6fa2f30cd74fefbcb356488622
jinchenxiangdan <jinchenxiangdan@gmail.com>
2021-01-20 22:56:00 -0700

    Update 1641-count_sorted_vowel_strings.py

--- Python Answers/1641-count_sorted_vowel_strings.py
+++ Python Answers/1641-count_sorted_vowel_strings.py
@@ -1,2 +1,11 @@
-from
-1641. Count Sorted Vowel Strings
+from typing import List
+
+def countVowelStrings_O1(self, n: int) -> int:
+    """
+    Author: Shawn Jin
+    O(1) method
+    """
+    #return comb(n+4, 4)
+    return (n + 1) * (n + 2) * (n + 3) * (n + 4) / 24
+    
+def countVowelStrings_On(self, n: int)
